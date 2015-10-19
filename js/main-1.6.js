bed6b0709f4bc7fc8c31af65b916e9a3d883c8c8
HTML5-App-Market <lagendre@gmail.com>
2015-10-19 16:44:38 +0800

    update context and dns

--- js/main-1.6.js
+++ js/main-1.6.js
@@ -91,3 +91,33 @@ $(function() {
 
     $('figure.responsive-image').picture();
 });
+
+function initMap() {
+    //初始畫一個LatLng物件(位置的物件，須以經緯度為參數加入)
+    var latlng = new google.maps.LatLng(24.160069,120.643771); 
+    var mapOptions = {
+        center: latlng, //目前地圖中央的位置(須放置LatLng物件)
+        zoom: 15, //地圖縮放程度
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    //設定Google Map會檢視於哪個區塊中，如下為id=map_canvas的html標籤區塊
+    map = new google.maps.Map(document.getElementById("gmap"), 
+        mapOptions);
+ 
+    //設定地圖標記提示框中的html內容
+    var contentString ='<div style="width:100px;color:black;">嘉宏❤津舫囍宴<br />台中僑園飯店<br />民國105年1月23日, 中午12:30</div>'; 
+ 	
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString //提示框中的內容
+    });
+	 
+    //建立一個marker物件(地圖標記點)
+    var marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+		title: '嘉宏❤津舫囍宴'
+    });
+     
+    //檢視(open)提示框並指向map物件裡的marker標記點
+    infowindow.open(map, marker);
+}
